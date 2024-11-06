9ea611ebccd17daf3b36606d2bd804abae9de60b
Reinhardt Botha <botha.wr@gmail.com>
2024-11-06 00:09:42 -0800

    feat: update blog content

--- next.config.js
+++ next.config.js
@@ -13,7 +13,7 @@ const ContentSecurityPolicy = `
   media-src *.s3.amazonaws.com;
   connect-src *;
   font-src 'self';
-  frame-src giscus.app https://www.arcgis.com;
+  frame-src giscus.app https://www.arcgis.com https://www.youtube.com https://storymaps.arcgis.com;
 `
 
 const securityHeaders = [
