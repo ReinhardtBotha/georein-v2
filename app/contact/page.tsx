72dabc60c65f59b95e2ae2fffc9195a75579e7f9
Reinhardt Botha <botha.wr@gmail.com>
2024-11-06 17:04:51 -0800

    feat: completed project content

--- app/contact/page.tsx
+++ app/contact/page.tsx
@@ -1,20 +1,14 @@
-import { Authors, allAuthors } from 'contentlayer/generated'
-import { MDXLayoutRenderer } from 'pliny/mdx-components'
-import AuthorLayout from '@/layouts/AuthorLayout'
-import { coreContent } from 'pliny/utils/contentlayer'
+import PageTitle from '@/components/PageTitle'
 import { genPageMetadata } from 'app/seo'
 
 export const metadata = genPageMetadata({ title: 'About' })
 
 export default function Page() {
-  const author = allAuthors.find((p) => p.slug === 'default') as Authors
-  const mainContent = coreContent(author)
-
   return (
     <>
-      <AuthorLayout content={mainContent}>
-        <MDXLayoutRenderer code={author.body.code} />
-      </AuthorLayout>
+      <div className="py-32 text-center">
+        <PageTitle>Coming Soon</PageTitle>
+      </div>
     </>
   )
 }
