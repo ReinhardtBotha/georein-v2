319971ddbb8be849f44d3c71ec29759b39dd9262
Reinhardt Botha <botha.wr@gmail.com>
2024-11-10 22:36:14 -0800

    feat: update logo and title spacing

--- components/Header.tsx
+++ components/Header.tsx
@@ -15,14 +15,12 @@ const Header = () => {
   return (
     <header className={headerClass}>
       <Link href="/" aria-label={siteMetadata.headerTitle}>
-        <div className="flex items-center justify-between">
+        <div className="flex items-center justify-between ">
           <div className="mr-3">
             <Logo />
           </div>
           {typeof siteMetadata.headerTitle === 'string' ? (
-            <div className="hidden h-6 text-2xl font-semibold sm:block">
-              {siteMetadata.headerTitle}
-            </div>
+            <div className="hidden text-2xl font-semibold sm:block">{siteMetadata.headerTitle}</div>
           ) : (
             siteMetadata.headerTitle
           )}
